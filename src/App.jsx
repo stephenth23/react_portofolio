210f8e8b96ec88eb4055b7712755f0aee0070dba
Stefan Turcu <stefan.turcu23@gmail.com>
2021-12-14 18:19:21 +0200

    creat menu, updatat culori, modificat hamburgerul

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import Portofolio from './components/Portofolio/Portofolio'
 import Contact from './components/Contact/Contact'
 import "./App.scss"
 import {useState} from "react"
+import Menu from './components/Menu/Menu'
 
 
 export default function App() {
-  const [menuOpen, setMenuOpen] = useState(true)
+  const [menuOpen, setMenuOpen] = useState(false)
   return (
     <div className='app'>
       <Navbar menuOpen = {menuOpen} setMenuOpen = {setMenuOpen}/>
+      <Menu menuOpen = {menuOpen} setMenuOpen = {setMenuOpen}/>
       <div className="sections">
         <Intro />
         <About />
